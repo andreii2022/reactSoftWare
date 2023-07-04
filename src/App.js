24ecc2fdf6287e9a67419d90ac4bd63f06141b2d
Andrii Nikulin <nikulin.and90@gmail.com>
2023-07-04 16:48:51 +0300

    here I add logic so that the data is displayed denamically

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import data from './mock-data.json'
 
 
 
 function App() {
+
+const [contacts, setContact] = useState(data)
+
   return (
-    <div className="App">
-    <input type="text" placeholder="Search..." className="Search "/>
-    
-    </div>
-  );
+<div className='app-container'>
+  <table >
+    <thead>
+      <tr>
+        <th>Company</th>   
+        <th>Model</th>
+        <th>VIN</th>
+        <th>Color</th>
+        <th>Year</th>
+        <th>Price</th>
+        <th>Availability</th>
+        
+      </tr>
+    </thead>
+    <tbody>
+      {contacts.map((contact) => (
+        <tr>
+        <td>{contact.car}</td>        
+        <td>{contact.car_model}</td>
+        <td>{contact.car_vin}</td>
+        <td>{contact.car_color}</td>
+        <td>{contact.car_model_year}</td>
+        <td>{contact.price}</td>
+        <td>{contact.availability}</td>
+       
+      </tr>
+      ))}
+      
+    </tbody>
+  </table>
+</div>
+
+
+
+
+  )
 }
 
 export default App;
+
+
+
+
+
+

4d2f7bebef1a525b266c84dbd2738c6aaa36419b
Andrii Nikulin <nikulin.and90@gmail.com>
2023-07-05 17:30:53 +0300

    fix companents searh

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,10 +1,87 @@
+// import React, { useState } from 'react';
+
+// function Search({ onSearch }) {
+//   const [searchTerm, setSearchTerm] = useState('');
+
+//   const handleSearch = () => {
+//     onSearch(searchTerm);
+//   };
+
+//   return (
+//     <div className="search">
+//       <input
+//         type="text"
+//         placeholder="Поиск..."
+//         value={searchTerm}
+//         onChange={(e) => setSearchTerm(e.target.value)}
+//       />
+//       <button onClick={handleSearch}>Искать</button>
+//     </div>
+//   );
+// }
+
+// export default Search;
+
+// import React, { useState } from 'react';
+
+// function Search({ onSearch }) {
+//   const [searchTerm, setSearchTerm] = useState('');
+
+//   const handleSearch = (event) => {
+//     const term = event.target.value;
+//     setSearchTerm(term);
+//     onSearch(term);
+//   };
+
+//   return (
+//     <div className="search">
+//       <input
+//         type="text"
+//         placeholder="Поиск..."
+//         value={searchTerm}
+//         onChange={handleSearch}
+//       />
+//     </div>
+//   );
+// }
+
+// export default Search;
+
+// import React, { useState } from 'react';
+
+// function Search({ onSearch }) {
+//   const [searchTerm, setSearchTerm] = useState('');
+
+//   const handleSearch = (event) => {
+//     const term = event.target.value.toLowerCase();
+//     setSearchTerm(term);
+//     onSearch(term);
+//   };
+
+//   return (
+//     <div className="search">
+//       <input
+//         type="text"
+//         placeholder="Поиск..."
+//         value={searchTerm}
+//         onChange={handleSearch}
+//         autoComplete="off"
+//       />
+//     </div>
+//   );
+// }
+
+// export default Search;
+
 import React, { useState } from 'react';
 
 function Search({ onSearch }) {
   const [searchTerm, setSearchTerm] = useState('');
 
-  const handleSearch = () => {
-    onSearch(searchTerm);
+  const handleSearch = (event) => {
+    const term = event.target.value.toLowerCase();
+    setSearchTerm(term);
+    onSearch(term);
   };
 
   return (
@@ -13,12 +90,16 @@ function Search({ onSearch }) {
         type="text"
         placeholder="Поиск..."
         value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
+        onChange={handleSearch}
+        autoComplete="off"
       />
-      <button onClick={handleSearch}>Искать</button>
     </div>
   );
 }
 
 export default Search;
+
+
+
+
 
